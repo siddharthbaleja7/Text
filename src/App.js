7d2ebbfab9f93f830a4f1a2692627d103fd6e937
Siddharth Baleja <siddharthbaleja31@gmail.com>
2024-08-26 14:48:08 +0530

    About us page Modification

--- src/App.js
+++ src/App.js
@@ -2,14 +2,16 @@ import logo from './logo.svg';
 import './App.css';
 import NavBar from './components/NavBar';
 import TextForm from './components/TextForm';
+import Accordion from './components/Accordion';
 
 function App() {
   return (
     <>
     <NavBar title="Text" />
     <div className="container my-3">
-      <h1>Enter the text to analyze</h1>
-      <TextForm />
+      {/* <h1>Enter the text to analyze</h1> */}
+      {/* <TextForm /> */}
+      <Accordion />
     </div>
     </>
   );

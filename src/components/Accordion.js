482dab6151889c3a090c2a1468026cf3b3e33139
Siddharth Baleja <siddharthbaleja31@gmail.com>
2024-08-28 13:05:54 +0530

    Fix issues

--- src/components/Accordion.js
+++ src/components/Accordion.js
@@ -8,18 +8,18 @@ export default function Accordion(props){
 
     let myystyle = {
         color: props.mode === 'dark' ? 'white' : 'black',
-        backgroundColor : props.mode === 'dark' ? '#042743' : 'white',
+        backgroundColor : props.mode === 'dark' ? 'rgb(36 74 104)' : 'white',
     }
     
 
     return(
-        <div className = "container" style={myystyle}>
+        <div className = "container" style={{color: props.mode === 'dark' ? 'white' : 'black'}}>
             <h1 className="my-3">About us</h1>
             <div className="accordion" id="accordionExample">
                 <div className="accordion-item" style={myystyle} >
                     <h2 className="accordion-header">
                     <button className="accordion-button" type="button" style={myystyle} data-bs-toggle="collapse" data-bs-target="#collapseOne" aria-expanded="true" aria-controls="collapseOne">
-                        Accordion Item #1
+                       <strong>Accordion Item #1</strong> 
                     </button>
                     </h2>
                     <div id="collapseOne" className="accordion-collapse collapse show" data-bs-parent="#accordionExample">
@@ -31,7 +31,7 @@ export default function Accordion(props){
                 <div className="accordion-item" style={myystyle}>
                     <h2 className="accordion-header">
                     <button className="accordion-button collapsed" type="button" style={myystyle} data-bs-toggle="collapse" data-bs-target="#collapseTwo" aria-expanded="false" aria-controls="collapseTwo">
-                        Accordion Item #2
+                       <strong>Accordion Item #2</strong> 
                     </button>
                     </h2>
                     <div id="collapseTwo" className="accordion-collapse collapse" data-bs-parent="#accordionExample">
@@ -43,7 +43,7 @@ export default function Accordion(props){
                 <div className="accordion-item" style={myystyle}>
                     <h2 className="accordion-header">
                     <button className="accordion-button collapsed" type="button" style={myystyle} data-bs-toggle="collapse" data-bs-target="#collapseThree" aria-expanded="false" aria-controls="collapseThree">
-                        Accordion Item #3
+                       <strong>Accordion Item #3</strong> 
                     </button>
                     </h2>
                     <div id="collapseThree" className="accordion-collapse collapse" data-bs-parent="#accordionExample">

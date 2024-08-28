30d8fc83a20024a84b4e700a989d29d055d2100e
Siddharth Baleja <siddharthbaleja31@gmail.com>
2024-08-28 13:09:55 +0530

    Fix the CLS error and fix some issue

--- src/components/Alert.js
+++ src/components/Alert.js
@@ -6,9 +6,11 @@ export default function Alert(props){
         return low.charAt(0).toUpperCase() + low.slice(1);
     }
     return(
-        props.alert && <div class={`alert alert-${props.alert.type} alert-dismissible fade show`} role="alert">
-            <strong>{capital(props.alert.type)}</strong> : {props.alert.msg}
-            <button type="button" class="btn-close" data-bs-dismiss="alert" aria-label="Close"></button>
+        <div style={{height: '50px'}}>
+            {props.alert && <div class={`alert alert-${props.alert.type} alert-dismissible fade show`} role="alert">
+                <strong>{capital(props.alert.type)}</strong> : {props.alert.msg}
+                <button type="button" class="btn-close" data-bs-dismiss="alert" aria-label="Close"></button>
+            </div>}
         </div>
     )
 }
